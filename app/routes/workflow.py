05676ba7183745cbce4becc99b8a3251e7eab820
Scott Rutz <scrutz@microsoft.com>
2025-04-09 14:11:20 -0500

    checkpoint

--- app/routes/workflow.py
+++ app/routes/workflow.py
@@ -3,11 +3,13 @@ from pydantic import BaseModel
 from app.models.api_models import ChatRequest
 from app.services.sk import SemanticKernelService
 from app.services.weather_agent_service import WeatherAgentService
+from app.services.chat_agent_service import ChatAgentService
 import asyncio
 router = APIRouter()
 
 sk_service = SemanticKernelService()
 weather_service = WeatherAgentService()
+chat_agent_service = ChatAgentService()
 
 class WorkflowInput(BaseModel):
     data: str
@@ -34,4 +36,12 @@ async def run_weather_workflow(input_data: ChatRequest):
     POST endpoint for executing a weather workflow.
     """
     result = await weather_service.run_weather_agent(input_data)
+    return {"result": result}
+
+@router.post("/agent/chat")
+async def run_weather_workflow(input_data: ChatRequest):
+    """
+    POST endpoint for executing a weather workflow.
+    """
+    result = await chat_agent_service.run_chat(input_data)
     return {"result": result}
---
+++ app/services/chat_agent_service.py
@@ -0,0 +1,82 @@
+import os
+from typing import List
+
+import semantic_kernel as sk
+from dotenv import load_dotenv
+from opentelemetry import trace
+
+from app.models.api_models import ChatRequest, ExecutionDiagnostics, RequestResult
+from app.prompts.file_service import FileService
+from app.services.weather_plugin import WeatherPlugin
+
+from semantic_kernel.agents import ChatCompletionAgent
+from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
+from semantic_kernel.contents import ChatMessageContent
+from semantic_kernel.contents.chat_history import ChatHistory
+from semantic_kernel.functions.kernel_arguments import KernelArguments
+from semantic_kernel.contents import  ChatMessageContent, FunctionCallContent
+from azure.identity.aio import DefaultAzureCredential
+from semantic_kernel.agents import AzureAIAgent, AzureAIAgentSettings, AzureAIAgentThread
+
+class ChatAgentService:
+    def __init__(self):
+        # Load environment variables from .env file
+        load_dotenv()
+
+        # Retrieve configuration from environment variables
+        api_key = os.getenv("AZURE_OPENAI_API_KEY")
+        endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+        deployment_name = os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT_NAME")
+        
+        if not api_key or not endpoint or not deployment_name:
+            raise ValueError("Missing required environment variables for OpenAI configuration.")
+    
+        self.ai_agent_settings = AzureAIAgentSettings.create()
+        self.file_service = FileService()
+
+        pass
+
+
+    async def run_chat(self, request: ChatRequest) -> str:
+        tracer = trace.get_tracer(__name__)
+        with tracer.start_as_current_span("Agent: Chat") as current_span:
+            # Validate the request object
+            if not request.messages:
+                raise ValueError("No messages found in request.")
+            user_message = request.messages[-1].content
+
+            async with (DefaultAzureCredential() as creds, AzureAIAgent.create_client(credential=creds) as client,):
+                
+                # 1. Create an agent on the Azure AI agent service
+                agent_definition = await client.agents.create_agent(
+                    model=self.ai_agent_settings.model_deployment_name,
+                    name="Assistant",
+                    instructions="Answer the user's questions.",
+                )
+
+                # 2. Create a Semantic Kernel agent for the Azure AI agent
+                agent = AzureAIAgent(
+                    client=client,
+                    definition=agent_definition,
+                )
+
+                # 3. Create a thread for the agent
+                # If no thread is provided, a new thread will be
+                # created and returned with the initial response
+                thread: AzureAIAgentThread = None
+
+                try:
+                    # 4. Invoke the agent with the specified message for response
+                    response = await agent.get_response(messages=user_message, thread=thread)
+                    thread = response.thread
+                finally:
+                    # 5. Cleanup: Delete the thread and agent
+                    await thread.delete() if thread else None
+                    await client.agents.delete_agent(agent.id) if agent else None   
+
+            request_result = RequestResult(
+                content=f"{response}")
+
+            return request_result
